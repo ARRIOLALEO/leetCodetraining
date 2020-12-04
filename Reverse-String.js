5760674d5a3c62f60ebbc8287eb3261d1326b5a2
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2020-12-04 07:51:00 +0600

    Create Reverse-String.js

---
+++ Reverse-String.js
@@ -0,0 +1,11 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    if(!s) {
+        return [];
+    }
+    
+    return s.reverse();
+};
