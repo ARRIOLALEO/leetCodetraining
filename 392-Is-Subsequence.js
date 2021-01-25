7db7315de6ca5bf9adc0190ca8653cf96645aca9
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2021-01-25 18:04:33 +0600

    Create 392-Is-Subsequence.js

---
+++ 392-Is-Subsequence.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let solution = [],
+        shadow = t,
+        startpoint = 0
+    for (let i = 0; i < s.length; i++) {
+        let sentinel = true
+        for (let j = startpoint; j < shadow.length; j++) {
+            if (s[i] === shadow[j]) {
+                if (sentinel === true) {
+                    solution.push([shadow[j], j])
+                    startpoint = j + 1
+                    sentinel = false
+                }
+            }
+        }
+
+    }
+    let prev = -Infinity
+    if (solution.length !== s.length) return false
+    for (let w = 0; w < solution.length; w++) {
+        if (prev < solution[w][1]) {
+            prev = solution[w][1]
+        } else {
+            return false
+            break
+        }
+
+    }
+    return true
+};
