44eef9a00606e64e78a26aedff07f228d08a34a1
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2021-07-10 08:27:40 +0600

    Create 704BinarySearch.js

---
+++ 704BinarySearch.js
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l , r = 0 , len(nums) -1
+        while l <= r:
+            middle = l + round((r - l) /2)
+            
+            if nums[middle] == target:
+                return middle
+            if nums[middle] < target:
+                l = middle +1
+            else:
+                r = middle -1
+            
+        return -1
+        
