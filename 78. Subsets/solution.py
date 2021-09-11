47e6a2b819bb115ba30b4d2aff3d4cb82421abcd
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2021-09-11 07:09:25 +0600

    Update solution.py

--- 78. Subsets/solution.py
+++ 78. Subsets/solution.py
@@ -8,5 +8,36 @@ class Solution:
         return solution
                 
                 
+class Solution(object):
+    def __init__(self):
+        self.solution =[]
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # def of backtracking function it is needed to know is helper arr is
+        #size of the required arr
+        def backtracking(start=0,helper =[]):
+        # eg is needed arr 0 is len(arr) == 0 append it to the solution
+            if len(helper) == k:
+                self.solution.append(helper[:])
+                return
+            else:
+                for i in range(start,s):
+                    #add the number to the helper arr
+                    helper.append(nums[i])
+                    # move one step fron
+                    backtracking(i+1,helper)
+                    # no duplicate remove the number used
+                    helper.pop()
+        s = len(nums)
+        ##+1 becuase i need an empty arr
+        for k in range(s+1):
+            # calling backtrack and k is the leng of the sub array needed
+            backtracking()
+        #return solution
+        return self.solution
+                
         
         
