4d2e9e5b9d9e926a1f5422e394f97ea72adfba4e
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2021-09-23 18:15:48 +0600

    Update 496NextGreaterElementI.py

--- 496NextGreaterElementI.py
+++ 496NextGreaterElementI.py
@@ -1,19 +1,25 @@
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        solution = []
-        dictionary = {}
-        for i in range(len(nums2)):
-            greater = nums2[i]
-            for j in range(i , len(nums2)):
-                if greater < nums2[j]:
-                    greater = nums2[j]
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        stack =[]
+        
+        next_greater = {}
+        
+        for index, n in enumerate(nums1):
+            ind = nums2.index(n)
+            temp = nums2[ind:]
+            for b in temp:
+                if b > n:
+                    next_greater[n] = b
                     break
-            if greater == nums2[i]:
-                dictionary[nums2[i]] = -1
-            else:
-                dictionary[nums2[i]] = greater
-        print(dictionary)
-        for element in nums1:
-            solution.append(dictionary[element])
+            if not n in next_greater:
+                next_greater[n]= -1
             
-        return solution
+        for n in nums1:
+            stack.append(next_greater[n])
+            
+        return stack
