3f7851f76599b2f931d380c83202078e6bda3b51
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2021-10-06 06:16:25 +0600

    Create solution.py

---
+++ 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree/solution.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def getTargetCopy(self, original, cloned, target):
+        """
+        :type original: TreeNode
+        :type cloned: TreeNode
+        :type target: TreeNode
+        :rtype: TreeNode
+        """
+        self.solution  = None
+        def bfs(o,c,t):
+            if  o:
+                dfs(o.left,c.left,t)
+                if o is t:
+                    self.solution = c
+                dfs(o.right,c.right,t)
+            
+        dfs(original,cloned, target)
+        return self.solution
+        
