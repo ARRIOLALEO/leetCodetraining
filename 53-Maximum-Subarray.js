26ee2abbb2d18d9968178329ba7ca88620e8ad01
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2021-11-08 06:51:08 +0600

    Update 53-Maximum-Subarray.js

--- 53-Maximum-Subarray.js
+++ 53-Maximum-Subarray.js
@@ -19,3 +19,31 @@ var maxSubArray = function(nums) {
     return maximunSume
     
 };
+
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        max_product = nums[0]
+        max_product_include_actual = 0
+        
+        for n in nums:
+            if (n + max_product_include_actual ) > n:
+                max_product_include_actual += n
+            else:
+                max_product_include_actual = n
+            
+            if max_product > max_product_include_actual :
+                pass
+            else:
+                max_product = max_product_include_actual
+
+            
+        return max_product
