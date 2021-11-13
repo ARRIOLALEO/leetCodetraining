f86abdae40bd8891441bbd30d38e91dc95a15d1b
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2021-11-13 09:15:10 +0600

    Update 198HouseRobber.py

--- 198HouseRobber.py
+++ 198HouseRobber.py
@@ -20,3 +20,20 @@ class Solution:
             max_robery[i] = max(max_robery[i-1], nums[i] + max_robery[i-2])
         return max_robery[-1]
             
+
+#cleaner solution
+
+    def rob(self, nums: List[int]) -> int:
+        number_of_houses = len(nums)
+        if number_of_houses == 1:
+            return nums[0]
+        if number_of_houses == 2:
+            return max(nums[0],nums[1])
+        
+        max_robbery = [nums[0],max(nums[0],nums[1])]
+        
+        for house in range(2,number_of_houses):
+            max_robbery.append(max(max_robbery[house-2]+nums[house],max_robbery[-1]))
+                              
+        return max(max_robbery[-1],max_robbery[-2])
+
